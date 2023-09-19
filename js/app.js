c2bd15217048f0288b98c2813b9dd74f08114114
Amantu Khan <amantu.khan@g.bracu.ac.bd>
2023-09-20 03:37:57 +0600

    weird things

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
-//Make a loaded that shows for 3 second before the page loads
 $(window).on('load', function() {
-    setInterval(function() {
-        $(".loader").fadeOut("slow");
+    setTimeout(function() {
+        $(".word" ).animate({
+            marginLeft: "100%",
+        }, 1000);
+        $(".word").fadeOut("slow");
+        $(".overlay").fadeOut("slow");
         loadGithubPage();
         removeopacity();
-    }, 2000);
+    }, 2500);
 });
 
 function loadGithubPage() {
@@ -64,7 +67,83 @@ $(document).ready(function() {
                 startRed = !startRed;
             }
         }
-        console.log(red, green, blue);
         $(".github-readme-stats-image").css("border", "2px solid rgb(" + red + "," + green + "," + blue);
     }, 1);
+});
+
+
+function Ticker( elem ) {
+	elem.lettering();
+	this.done = false;
+	this.cycleCount = 5;
+	this.cycleCurrent = 0;
+	this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+{}|[]\\;\':"<>?,./`~'.split('');
+	this.charsCount = this.chars.length;
+	this.letters = elem.find( 'span' );
+	this.letterCount = this.letters.length;
+	this.letterCurrent = 0;
+
+	this.letters.each( function() {
+		var $this = $( this );
+		$this.attr( 'data-orig', $this.text() );
+		$this.text( '-' );
+	});
+}
+
+Ticker.prototype.getChar = function() {
+	return this.chars[ Math.floor( Math.random() * this.charsCount ) ];
+};
+
+Ticker.prototype.reset = function() {
+	this.done = false;
+	this.cycleCurrent = 0;
+	this.letterCurrent = 0;
+	this.letters.each( function() {
+		var $this = $( this );
+		$this.text( $this.attr( 'data-orig' ) );
+		$this.removeClass( 'done' );
+	});
+	this.loop();
+};
+
+Ticker.prototype.loop = function() {
+	var self = this;
+
+	this.letters.each( function( index, elem ) {
+		var $elem = $( elem );
+		if( index >= self.letterCurrent ) {
+			if( $elem.text() !== ' ' ) {
+				$elem.text( self.getChar() );
+				$elem.css( 'opacity', Math.random() );
+			}
+		}
+	});
+
+	if( this.cycleCurrent < this.cycleCount ) {
+		this.cycleCurrent++;
+	} else if( this.letterCurrent < this.letterCount ) {
+		var currLetter = this.letters.eq( this.letterCurrent );
+		this.cycleCurrent = 0;
+		currLetter.text( currLetter.attr( 'data-orig' ) ).css( 'opacity', 1 ).addClass( 'done' );
+		this.letterCurrent++;
+	} else {
+		this.done = true;
+	}
+
+	if( !this.done ) {
+		requestAnimationFrame( function() {
+			self.loop();
+		});
+	} else {
+		setTimeout( function() {
+			self.reset();
+		}, 750 );
+	}
+};
+
+words = $( '.word' );
+words.each( function() {
+    var thiselem = $( this ),
+    ticker = new Ticker( thiselem ).reset();
+    thiselem.data( 'ticker', ticker );
 });
