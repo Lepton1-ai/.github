e9cd96fe43684dd69dcbfbfd70a878abfb2285ad
Hridoy <19202103103@cse.bubt.edu.bd>
2023-09-22 23:21:55 +0600

    tolocaltimestring

--- lepton1/src/Digital_clock.tsx
+++ lepton1/src/Digital_clock.tsx
@@ -1,26 +1,17 @@
-import {useState} from "react";
-import './time.css';
-function CLOCK(){
+import { useState } from "react";
+import "./time.css";
+export default CLOCK = () => {
+  let time = new Date().toLocaleTimeString();
+  const [curr_time, set_time] = useState(time);
+  const get_time = () => {
+    let time = new Date().toLocaleTimeString();
+    set_time(time);
+  };
+  setInterval(get_time, 1000);
 
-    const time =new Date().toLocaleDateString();
-    const [curr_time,set_time]=useState(time);
-    const get_time = () =>{
-        const time =new Date().toLocaleDateString();
-        set_time(time)
-    }
-    setInterval(get_time,1000);
-
-    return (
-        <>
-            <div className="digital_clock">
-                <h1>
-                    {curr_time}
-                </h1>
-            </div>
-        </>
-
-    );
-}
-
-
-export default CLOCK
+  return (
+    <div className="digital_clock">
+      <h1>{curr_time}</h1>
+    </div>
+  );
+};
