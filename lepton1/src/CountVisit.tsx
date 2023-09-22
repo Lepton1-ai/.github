1fd04260ada08dbc87225e6f4a7a0f5f94f40fc0
Amantu Khan <amantu.khan@g.bracu.ac.bd>
2023-09-22 22:16:19 +0600

    ditching JSON

--- lepton1/src/CountVisit.tsx
+++ lepton1/src/CountVisit.tsx
@@ -1,14 +1,17 @@
 import saveinfo from './saveinfo.json';
-import saveAs from 'file-saver';
+import { getDatabase } from "firebase/database";
 
 const CountVisit = (count: any) => {
+    const firebaseConfig = {
+        databaseURL: "https://lepton-github-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    }
+    const database = getDatabase();
     if (count === false) {
         return saveinfo.count
     }
     saveinfo.count = count;
     console.log(saveinfo.count);
-    const blob = new Blob([JSON.stringify(saveinfo)], {type: "application/json"});
-    saveAs(blob, "saveinfo.json");
+    
     return saveinfo.count;
 };
 
