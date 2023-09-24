e2850441257f6500025de9e04a859dfec8911cb7
Amantu Khan <amantu.khan@g.bracu.ac.bd>
2023-09-24 21:11:22 +0600

    aading copywrite footer

---
+++ lepton1/src/components/footer/copywriteFooter.tsx
@@ -0,0 +1,17 @@
+import './copywriteFooter.scss';
+import React from 'react';
+
+function CopywriteFooter() {
+    return (
+        <>
+            <footer className="footer">
+                <div className="copywrite-footer">
+                    <p>© 2023 Lepton1. All rights reserved.</p>
+                </div>
+            </footer>
+        </>
+
+    );
+}
+
+export default CopywriteFooter;
--- lepton1/src/main.tsx
+++ lepton1/src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import CopywriteFooter from './components/footer/copywriteFooter.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <CopywriteFooter />
   </React.StrictMode>,
 )
