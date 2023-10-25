79ce967e341d7d84faec9faceceb5fdadfd6d6bf
Hridoy <19202103103@cse.bubt.edu.bd>
2023-10-25 12:56:11 +0600

    Work Experience section add

--- lepton1/src/App.tsx
+++ lepton1/src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from './components/navbar/Navbar.tsx'
 import Header from './components/home/Home.tsx'
+import Experience from './components/experience/Experience.tsx'
 import './App.scss'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <div>
         <Navbar/>
         <Header/>
+        <Experience/>
       </div>
     </>
   )
---
+++ lepton1/src/components/constants/index.js
@@ -0,0 +1,59 @@
+import figma from '/src/assets/tech/figma.png'
+import reactjs from '/src/assets/tech/reactjs.png'
+import redux from '/src/assets/tech/redux.png'
+import javascript from '/src/assets/tech/javascript.png'
+    export const experiences = [
+        {
+          title: "React.js Developer",
+          company_name: "AIMANGO",
+          icon: reactjs,
+          iconBg: "#383E56",
+          date: "March 2023 - Present",
+          points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+          ],
+        },
+        {
+          title: "React Native Developer",
+          company_name: "AIMANGO",
+          icon: redux,
+          iconBg: "#E6DEDD",
+          date: "March 2023 - Present",
+          points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+          ],
+        },
+        {
+          title: "Web Developer",
+          company_name: "Fish Vally",
+          icon: javascript,
+          iconBg: "#383E56",
+          date: "Jan 2020 - Jan 2021",
+          points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+          ],
+        },
+        {
+          title: "Web Designer",
+          company_name: "AIMANGO",
+          icon: figma,
+          iconBg: "#E6DEDD",
+          date: "Jan 2023 - Present",
+          points: [
+            "Figma is a cloud-based platform, which means your design files are stored in the cloud and can be accessed from anywhere with an internet connection.",
+            "This facilitates collaboration among team members who can simultaneously work on the same design file.",
+            "Figma has an active online community, and many designers share their resources and templates.",
+            "Figma supports a wide range of plugins and integrations with other design and development tools. This makes it versatile and adaptable to your specific design workflow.",
+          ],
+        },
+      ];
+      
